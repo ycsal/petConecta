1be27c903ee059f6d4f3229a537ca35998cdccd5
ycsal <yascsalgado@gmail.com>
2025-09-23 12:16:39 -0300

    rascunho footer

---
+++ components/FooterMenu/index.jsx
@@ -0,0 +1,11 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+
+export const FooterMenu = ({ onPress, title, icon }) => {
+    return (
+    )
+} 
+
+const styles = StyleSheet.create({
+
+}); 
+//falta fazer -- yas estudando conteúdo
