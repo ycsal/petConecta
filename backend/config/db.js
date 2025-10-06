cc599ca5395e800e316d891e238a6dc4b14c483a
HOLTZ <nathan.holtz0805@gmail.com>
2025-10-06 19:44:11 -0300

    modified:   backend/config/db.js 	modified:   backend/server.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-// Pegue sua String de Conexão no site do MongoDB Atlas
-// DICA: Em "Network Access" no Atlas, libere o acesso de qualquer IP (0.0.0.0/0) para os testes.
+
 const mongoURI = 'mongodb+srv://nathanholtz:ABNYfatec@petconecta.aotiaz7.mongodb.net/?retryWrites=true&w=majority&appName=petconecta';
 
 const connectDB = async () => {
   try {
-    // Tenta conectar ao banco de dados
+    // Conecta ao MongoDB usando o mongoose
     await mongoose.connect(mongoURI);
     console.log('✅ Conectado com sucesso ao MongoDB!');
   } catch (err) {
-    // Se der erro, exibe o erro e encerra o processo do servidor
     console.error('❌ Erro ao conectar ao MongoDB:', err.message);
-    process.exit(1); // Encerra a aplicação com um código de falha
+    process.exit(1);
   }
 };
 
